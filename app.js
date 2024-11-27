9c552d462d4babd035d09212d5901deab71301f5
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-27 12:53:11 +0900

    ok

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 const express = require('express')
 const app = express()
 const logger = require('./logger')
+const authorize = require('./authorize')
 
 
-app.use(logger)
+app.use([logger,authorize])
 
 
 app.get('/',(req,res) => {
     res.send('Home page')
+    console.log(req.user);
+    
 })
 
 
