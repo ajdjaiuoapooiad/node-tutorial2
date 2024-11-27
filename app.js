fb117643a20bc84a83fb0e192a591a23d852a3a5
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-27 14:10:58 +0900

    ok

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
+let {people} = require('./data')
 
 
-app.use([logger,authorize])
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
 
-
-app.get('/',(req,res) => {
-    res.send('Home page')
-    console.log(req.user);
+app.get('/api/people',(req,res) => {
+    res.status(200).json({success: true,data: people})
     
+})
+
+app.post('/login',(req,res) => {
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Error occered')
+
 })
 
 
