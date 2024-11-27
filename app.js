1d428f11986d8e9b4b1051f29387a2d9ae7c048f
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-27 10:44:45 +0900

    ok

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 const express = require('express')
 const app = express()
+const path = require('path')
+
+app.use(express.static('./public'))
 
 
 app.get('/',(req,res) => {
-    console.log();
-    res.status(200).send('Home page')
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
     
 })
 app.get('/about',(req,res) => {
