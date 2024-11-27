0086c08b6713d9bf3c7391682f8f4996261a8db8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-27 11:05:27 +0900

    67ch ok

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 
 const express = require('express')
 const app = express()
-const path = require('path')
-
-app.use(express.static('./public'))
+const {products} = require('./data')
 
 
 app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-    
+    res.json(products)
 })
-app.get('/about',(req,res) => {
-    console.log();
-    res.status(200).send('About page')
-    
-})
+
 
 app.listen(5000, () => {
     console.log('server is running .....');
